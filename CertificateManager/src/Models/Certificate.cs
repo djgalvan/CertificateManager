6401ea568ff62736635cf77df631269604c26111
David J Galvan Jr <djgalvan01@outlook.com>
2023-03-12 12:47:30 -0400

    Restore GetCert method

--- CertificateManager/src/Models/Certificate.cs
+++ CertificateManager/src/Models/Certificate.cs
@@ -1,7 +1,37 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace CertificateManager
 {
     internal class Certificate
     {
+        private static readonly string TestCertificate = Path.Combine(
+            Environment.GetEnvironmentVariable("PKICertDir"), "certificate.p12"
+        );
+
+        private static readonly X509Certificate x509Certificate = X509Certificate.CreateFromCertFile(TestCertificate);
+
+        internal static Certificate GetCertificate()
+        {
+            Certificate certificate = new Certificate
+            {
+                Hash = x509Certificate.GetCertHash(),
+                EffectiveDate = DateTime.Parse(x509Certificate.GetEffectiveDateString()),
+                ExpirationDate = DateTime.Parse(x509Certificate.GetExpirationDateString()),
+                Format = x509Certificate.GetFormat(),
+                HashCode = x509Certificate.GetHashCode(),
+
+                IssuerName = x509Certificate.GetIssuerName(),
+                KeyAlgorithm = x509Certificate.GetKeyAlgorithm(),
+                KeyAlgorithmParameters = x509Certificate.GetKeyAlgorithmParameters(),
+
+                Principal = x509Certificate.GetName(),
+                PublicKey = x509Certificate.GetPublicKey(),
+                RawCertData = x509Certificate.GetRawCertData(),
+                SerialNumber = x509Certificate.GetSerialNumber(),
+            };
+            return certificate;
+        }
+
         public int CertificateId { get; set; }
 
         public byte[] Hash { get; set; }
